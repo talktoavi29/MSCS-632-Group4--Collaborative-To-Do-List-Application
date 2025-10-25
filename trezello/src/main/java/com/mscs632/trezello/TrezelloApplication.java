bf5f6625ae04471482dc2380b798ad53303efc4e
talktoavi29 <37131576+talktoavi29@users.noreply.github.com>
2025-10-25 10:14:46 -0500

    project initial structure build

---
+++ trezello/src/main/java/com/mscs632/trezello/TrezelloApplication.java
@@ -0,0 +1,13 @@
+package com.mscs632.trezello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrezelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrezelloApplication.class, args);
+	}
+
+}
